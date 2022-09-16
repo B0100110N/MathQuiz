bc9c5bfcaac2352880fdd3568fae59ad52dcf883
B0100110N <102432152+B0100110N@users.noreply.github.com>
2022-09-16 00:09:15 -0500

    Add files via upload

---
+++ math_quiz.py
@@ -0,0 +1,14 @@
+#math quiz
+name = ''
+
+
+def greet_player():
+    print("Hello! Welcome to Ben's Simple Math Quiz.")
+
+
+def get_name():
+    input('What is your name?')
+
+
+greet_player()
+get_name()
